3a85b91cdc1f9c0f6aeb5b5bf51677cb4ce30dd3
DenisSvinkovskij <1911.denis55@gmail.com>
2021-04-15 09:39:08 +0300

    done hw-04

--- src/repository/contacts.js
+++ src/repository/contacts.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const Contact = require('../schemas/schema-contact');
 
 class ContactsRepository {
@@ -5,47 +6,73 @@ class ContactsRepository {
     this.contactModel = Contact;
   }
 
-  async listContacts() {
-    const result = await this.contactModel.find({});
-    console.log(result);
+  async listContacts(userId, { limit = 10, page = 1, favorite }) {
+    const result = await this.contactModel.paginate(
+      { owner: userId },
+      {
+        limit,
+        page,
+        // пытался отфильтровать только favorite=true...получилось только через колбэк ниже...
+        // filter: {
+        //   ...(favorite ? { favorite: true } : {}),
+        // },
+        populate: {
+          path: 'owner',
+          select: 'subscription email -_id',
+        },
+      },
+      (err, result) => {
+        if (favorite) {
+          return {
+            ...result,
+            docs: result.docs.filter(item => item.favorite === true),
+          };
+        }
+        return result;
+      },
+    );
     return result;
   }
 
-  async getContactById(contactId) {
-    const result = await this.contactModel.findOne({ _id: contactId });
+  async getContactById(userId, contactId) {
+    const result = await this.contactModel
+      .findById(contactId, { owner: userId })
+      .populate({
+        path: 'owner',
+        select: 'subscription email -_id',
+      });
     return result;
   }
 
-  async removeContact(contactId) {
-    const record = await this.contactModel.findByIdAndRemove({
-      _id: contactId,
+  async removeContact(userId, contactId) {
+    const record = await this.contactModel.findByIdAndRemove(contactId, {
+      owner: userId,
     });
 
     return record;
   }
 
-  async addContact(body) {
-    const newContact = await this.contactModel.create(body);
+  async addContact(userId, body) {
+    const newContact = await this.contactModel.create({
+      ...body,
+      owner: userId,
+    });
 
     return newContact;
   }
 
-  async updateContact(contactId, body) {
+  async updateContact(userId, contactId, body) {
     const updatedContact = await this.contactModel.findByIdAndUpdate(
-      {
-        _id: contactId,
-      },
+      { _id: contactId, owner: userId },
       { ...body },
       { new: true },
     );
     return updatedContact;
   }
 
-  async updateContactStatus(contactId, body) {
+  async updateContactStatus(userId, contactId, body) {
     const updatedContact = await this.contactModel.findByIdAndUpdate(
-      {
-        _id: contactId,
-      },
+      { _id: contactId, owner: userId },
       { ...body },
       { new: true },
     );
